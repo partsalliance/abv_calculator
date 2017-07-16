983d5b647d907514ee36d71beac0e17210225f27
partsalliance <patrickcroot@gmail.com>
2017-07-16 16:17:09 +0100

    abv endpoint created

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"io"
 	"net/http"
+	"strconv"
 )
 
 func abv(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "abv calculator")
+
+	og_string := req.FormValue("og")
+	og, err := strconv.ParseFloat(og_string, 64)
+	if err != nil {
+		// do something sensible
+	}
+
+	fg_string := req.FormValue("fg")
+	fg, err := strconv.ParseFloat(fg_string, 64)
+	if err != nil {
+		// do something sensible
+	}
+	abv_value := (og - fg) * 131.25
+	string_abv_value := strconv.FormatFloat(abv_value, 'f', 2, 64)
+
+	io.WriteString(res, string_abv_value + "%")
 }
 
 
